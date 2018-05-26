5d6a92b577b8e4b738ce5e709197de760800cf16
Giovanni Silva <giovannigatfut@gmail.com>
2018-05-26 00:04:28 -0500

    centrando los iconos de los labels

--- application/views/categorias/preguntas_energia.php
+++ application/views/categorias/preguntas_energia.php
@@ -2,15 +2,15 @@
 	<h1 align="center">¿Cómo se crea la energia?</h1>
           <div class="quiz" id="quiz" data-toggle="buttons">
 
-            <label class="element-animation1 btn btn-lg btn-success btn-block milabel"><span class="btn-label"><i class="glyphicon glyphicon-leaf"></i></span><input type="submit"hidden="true"><h3 align="center">1</h3></label>
+            <label class="element-animation1 btn btn-lg btn-success btn-block milabel"><span class="btn-label"><i class="glyphicon glyphicon-leaf" style="font-size: 30px"></i></span><input type="submit"hidden="true"><h3 align="center">1</h3></label>
 
-            <label class="element-animation1 btn btn-lg btn-success btn-block milabel"><span class="btn-label"><i class="glyphicon glyphicon-leaf"></i></span><input type="submit"hidden="true"><h3 align="center">2</h2></label>
+            <label class="element-animation1 btn btn-lg btn-success btn-block milabel"><span class="btn-label"><i class="glyphicon glyphicon-leaf" style="font-size: 30px"></i></span><input type="submit"hidden="true"><h3 align="center">2</h2></label>
             
-             <label class="element-animation1 btn btn-lg btn-success btn-block milabel"><span class="btn-label"><i class="glyphicon glyphicon-leaf"></i></span><input type="submit"hidden="true"><h3 align="center">3</h3></label>
+             <label class="element-animation1 btn btn-lg btn-success btn-block milabel"><span class="btn-label"><i class="glyphicon glyphicon-leaf" style="font-size: 30px"></i></span><input type="submit"hidden="true"><h3 align="center">3</h3></label>
 
-             <label class="element-animation1 btn btn-lg btn-success btn-block milabel"><span class="btn-label"><i class="glyphicon glyphicon-leaf"></i></span><input type="submit"hidden="true"><h3 align="center">4</h3> </label>
+             <label class="element-animation1 btn btn-lg btn-success btn-block milabel"><span class="btn-label"><i class="glyphicon glyphicon-leaf" style="font-size: 30px"></i></span><input type="submit"hidden="true"><h3 align="center">4</h3> </label>
   
-            <label class="element-animation1 btn btn-lg btn-success btn-block milabel"><span class="btn-label"><i class="glyphicon glyphicon-leaf"></i></span><input type="submit"hidden="true"><h3 align="center">5</h3> </label>
+            <label class="element-animation1 btn btn-lg btn-success btn-block milabel"><span class="btn-label"><i class="glyphicon glyphicon-leaf" style="font-size: 30px"></i></span><input type="submit"hidden="true"><h3 align="center">5</h3> </label>
           </div>    
         </div>
       </div>
