53adebde7418870d779a645b66b6fbda1a679121
Giovanni Silva <giovannigatfut@gmail.com>
2018-05-30 22:43:48 -0500

    iniciando con la programación del microcontrolador

--- application/views/preguntas/curluz/1.php
+++ application/views/preguntas/curluz/1.php
@@ -4,10 +4,10 @@
          </div>
             <div class="quiz" id="quiz" data-toggle="buttons">
             <form action="<?php echo base_url(); ?>">
-              <label class="element-animation1 btn btn-lg btn-success btn-block milabel"><span class="btn-label"><i class="glyphicon fa fa-plug icono" style="font-size: 30px; top: 30%;"></i></span><input type="submit"hidden="true" name="true"><h4 class="icono">Cierto</h4></label>
+              <label class="element-animation1 btn btn-lg btn-success btn-block milabel"><span class="btn-label"><i class="glyphicon fa fa-plug icono" style="font-size: 30px; top: 30%;"></i></span><input type="submit"hidden="true" name="curluz" value="cierto1"><h4 class="icono">Cierto</h4></label>
             </form>
             <form action="<?php echo base_url(); ?>">
-              <label class="element-animation2 btn btn-lg btn-danger btn-block milabel"><span class="btn-label"><i class="glyphicon fa fa-plug icono" style="font-size: 30px; top: 30%;"></i></span> <input type="submit" hidden="true"><h4 class="icono">Falso</h4></label>
+              <label class="element-animation2 btn btn-lg btn-danger btn-block milabel"><span class="btn-label"><i class="glyphicon fa fa-plug icono" style="font-size: 30px; top: 30%;"></i></span> <input type="submit" hidden="true" name="curluz" value="falso1"><h4 class="icono">Falso</h4></label>
             </form>
           </div>    
         </div>
